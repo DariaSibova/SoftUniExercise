311b02fbd8cdb6ca5480ccd5b73ff9175fbe03be
Daria Sibova <dariasib05@gmail.com>
2023-09-25 17:15:52 +0300

    commit

---
+++ Basic Syntax/theatrePromotions.js
@@ -0,0 +1,31 @@
+function tickets(day, age) {
+    let price = 0;
+    if (day == 'Weekday') {
+        if ((age >= 0 && age <= 18) || (age > 64 && age <= 122)) {
+            price = 12
+        } else if (age > 18 && age <= 64) {
+            price = 18;
+        }
+    } else if (day == 'Weekend') {
+        if ((age >= 0 && age <= 18) || (age > 64 && age <= 122)) {
+            price = 15;
+        } else if (age > 18 && age <= 64) {
+            price = 20;
+        }
+    } else if (day == 'Holiday') {
+        if (age >= 0 && age <= 18) {
+            price = 5;
+        } else if (age > 64 && age <= 122) {
+            price = 10;
+        } else if (age > 18 && age <= 64) {
+            price = 12;
+        }
+    } if(price != 0){
+        console.log(price + '$')
+    }else{
+        console.log('Error!')
+    }
+}
+tickets('Weekday', 42);
+// tickets('Holiday',-12);
+// tickets('Holiday',15);

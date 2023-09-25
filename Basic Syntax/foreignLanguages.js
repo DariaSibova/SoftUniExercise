478fb8d0a451a1006e54085eb2112b02fe842c49
Daria Sibova <dariasib05@gmail.com>
2023-09-25 14:23:46 +0300

    exercise 3

---
+++ Basic Syntax/foreignLanguages.js
@@ -0,0 +1,25 @@
+function solve(country) {
+
+    switch (country) {
+        case 'USA':
+            console.log('English')
+            break;
+        case 'England':
+            console.log('English')
+            break;
+        case 'Spain':
+            console.log('Spanish');
+            break;
+        case 'Argentina':
+            console.log('Spanish');
+            break;
+        case 'Mexico':
+            console.log('Spanish');
+            break;
+        default:
+            console.log('unknown')
+            break;
+    }
+}
+solve('USA');
+// solve('Germany');

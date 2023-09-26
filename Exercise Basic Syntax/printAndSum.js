10ec256014cab584669ef9c129b42408b7c3379e
Daria Sibova <dariasib05@gmail.com>
2023-09-26 16:48:50 +0300

    commit

--- Exercise Basic Syntax/printAndSum.js
+++ Exercise Basic Syntax/printAndSum.js
@@ -1,3 +1,16 @@
-function solve(){
-    
+function solve(start,end){
+
+    let numsStr = ' '
+    let sum = 0;
+
+    for(let num = start; num <= end;num++){
+        numsStr += num + ' ';
+        sum += num;
+    }
+    console.log(numsStr);
+    console.log(`Sum: ${sum}`);
 }
+
+solve(5,10);
+// solve(0,26);
+// solve(50,60);
---
+++ Exercise Basic Syntax/triangleOfNumbers.js
@@ -0,0 +1,6 @@
+function solve(){
+
+}
+solve(3);
+// solve(5);
+// solve(6);

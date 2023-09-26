004f243b01a10257362ba8cffe08bbe305414aa8
Daria Sibova <dariasib05@gmail.com>
2023-09-26 16:10:14 +0300

    commit

--- Exercise Basic Syntax/vacation.js
+++ Exercise Basic Syntax/vacation.js
@@ -1,4 +1,48 @@
-function solve(){
-    
+function solve(count, type, day) {
+    let singularPrice = 0;
+
+    switch (day) {
+        case 'Friday':
+            if (type == 'Students') {
+                singularPrice = 8.45;
+            } else if (type == 'Business') {
+                singularPrice = 10.90;
+            } else if (type == 'Regular') {
+                singularPrice = 15;
+            }
+         break;
+        case 'Satuday':
+            if (type == 'Students') {
+                singularPrice = 9.80;
+            } else if (type == 'Business') {
+                singularPrice = 15.60;
+            } else if (type == 'Regular') {
+                singularPrice = 20;
+            }
+        break;
+        case 'Sunday':
+            if (type == 'Students') {
+                singularPrice = 10.46;
+            } else if (type == 'Business') {
+                singularPrice = 16;
+            } else if (type == 'Regular') {
+                singularPrice = 22.50;
+            }
+        break;
+    }
+
+    let totalPrice = count * singularPrice;
+
+    if(type == 'Student' && count >= 30){
+        totalPrice *= 0.85;
+    }else if(type == 'Business' && count >= 100){
+        let discountedPrice = 10 * singularPrice;
+        totalPrice -=  discountedPrice;
+    }else  if (type == 'Regular' && (count >= 10 && count <=20)){
+        totalPrice *= 0.95;
+    }
+
+    console.log(`Total price: ${totalPrice.toFixed(2)}`)
 }
-solve()
+solve(30, "Students", "Sunday");
+// solve(40,"Regular","Saturday")

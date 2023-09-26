c2297f26ba3fe7acd3596b8696f9ae8f4f019121
Daria Sibova <dariasib05@gmail.com>
2023-09-26 16:35:48 +0300

    commit

--- Exercise Basic Syntax/leapYear.js
+++ Exercise Basic Syntax/leapYear.js
@@ -1,4 +1,9 @@
-function solve(){
+function solve(year) {
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        console.log('yes');
+    } else {
+        console.log('no');
+    }
 
 }
 solve(1984);

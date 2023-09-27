23029ff51a6a9bb15f77a7066faecbd68a7ce40f
Daria Sibova <dariasib05@gmail.com>
2023-09-27 15:06:26 +0300

    commit

--- FUNDAMENTALS/Basic Syntax Exercise/login.js
+++ FUNDAMENTALS/Basic Syntax Exercise/login.js
@@ -1,25 +1,31 @@
-// You will be given a string representing a username. The correct password will be that username reversed.
-// Until you receive the correct password print on the console: "Incorrect password. 
-//Try again.". When you receive the correct password print: "User {username} logged in."
-
-// However, on the fourth try if the password is still not correct print: 
-//"User {username} blocked!" and end the program.
-
-// The input comes as an array of strings -
-// the first string represents username and each subsequent string is a password.
-
 function solve(input){
+ let username = input[0];
+ let pass = '';
+
+ for (let i = username.length - 1;i >= 0; i--){
+    pass += username[1];
+ }
+
+ let guess = input[1];
+ let index = 2;
+ let wrongAttempts = 0;
+ 
+ while(guess != pass){
+    wrongAttempts++;
+
+    if(wrongAttempts == 4){
+        console.log(`User ${username} blocked!`);
+        return;
+    }
+    console.log("Incorrect password. Try again.");
+
+    guess = input[index];
+    index++;
+ }
+ console.log(`User ${username} logged in.`)
 
 }
 
 solve(['Acer','login','go','let me in','recA']) //Incorrect password. 
-//Try again. Incorrect password.
-// Try again. Incorrect password. Try again. User Acer logged in.
-
 // solve(['momo','omom'])
-//User momo logged in.
-
 // solve(['sunny','rainy','cloudy','sunny','not sunny'])
-//Incorrect password.
-// Try again. Incorrect password. Try again. Incorrect password.
-// Try again. User sunny blocked!

eea86492792846967475b3e327dbde67c19386d9
Daria Sibova <dariasib05@gmail.com>
2023-09-29 16:08:10 +0300

    commit

--- FUNDAMENTALS/Data Types and Variables/integarAndFloat.js
+++ FUNDAMENTALS/Data Types and Variables/integarAndFloat.js
@@ -1,8 +1,18 @@
 function solve(num1,num2,num3){
-let sum = num1 + num2 + num3;
+// let sum = num1 + num2 + num3;
 
-sum % 1 === 0? sum += ' - Integer' : sum += ' - Float';
-console.log(sum)
+// sum % 1 === 0? sum += ' - Integer' : sum += ' - Float';
+// console.log(sum)
+let sum = num1 + num2 + num3;
+let sumAsString = String(sum);
+let isFloat = false;
+
+for(let i =0;i < sumAsString.length;i++){
+    if(sumAsString[i] == '.'){
+        isFloat = true;
+    }
+}
+console.log(`${sum} - ${isFloat ? 'Float' : 'Integer'}`)
 }
 
 solve(9,100,1.1);

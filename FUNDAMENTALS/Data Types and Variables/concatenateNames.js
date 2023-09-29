939d9af233bfb9afac9baa2a22154ee37c13a52d
Daria Sibova <dariasib05@gmail.com>
2023-09-29 15:25:17 +0300

    commit

---
+++ FUNDAMENTALS/Data Types and Variables/concatenateNames.js
@@ -0,0 +1,6 @@
+function solve(first,second,del){
+     console.log(`${first}${del}${second}`);
+}
+solve('John','Smith','->');
+// solve('Jan','White','<->');
+// solve('Linda','Terry','=>');
---
+++ FUNDAMENTALS/Data Types and Variables/demo.js
@@ -0,0 +1,8 @@
+function demo(){
+    let a = 'Hello world!';
+
+    for(let i =0;i < 5;i++){
+        console.log(a[i])
+    }
+}
+demo()

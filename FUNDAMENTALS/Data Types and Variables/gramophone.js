ac6f300157b2e531e3a1e5333cadeac60a8ed225
Daria Sibova <dariasib05@gmail.com>
2023-09-29 16:29:30 +0300

    commit

---
+++ FUNDAMENTALS/Data Types and Variables/gramophone.js
@@ -0,0 +1,10 @@
+function solve(bandName,albumName,songName){
+let time =(bandName.length*albumName.length)
+* songName.length/2;
+
+let rotation = Math.ceil(time/2.5);
+console.log(`The plate was rotated ${rotation} times.`)
+}
+
+solve('Black Sabbath', 'Paranoid','War Pigs')
+// solve('Rammstein', 'Sehnsucht','Engel')

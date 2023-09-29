0e3093ed3b837153708badb115ce3efe68c576cf
Daria Sibova <dariasib05@gmail.com>
2023-09-29 16:16:34 +0300

    commit

--- FUNDAMENTALS/Data Types and Variables/amazingNumbers.js
+++ FUNDAMENTALS/Data Types and Variables/amazingNumbers.js
@@ -1,13 +1,30 @@
 function solve(num){
-num = num.toString();
-let sum = 0
-for(let i = 0;i < num.length;i++){
-    sum += Number(num[i]);
+// num = num.toString();
+// let sum = 0
+// for(let i = 0;i < num.length;i++){
+//     sum += Number(num[i]);
+// }
+// let result = sum.toString().includes('9');
+// console.log(result ?
+//     `${num} Amazing? True` :
+//     `${num} Amazing? False`);
+
+let numAsString = String(num);
+let sum = 0;
+
+for(let i =0;i < numAsString.length;i++){
+    sum += Number(numAsString[i]);
+
 }
-let result = sum.toString().includes('9');
-console.log(result ?
-    `${num} Amazing? True` :
-    `${num} Amazing? False`);
+let sumAsString = String(sum);
+let isAmazing = 'False';
+
+for(let i =0;i< sumAsString.length;i++){
+    if(sumAsString[i] == '9'){
+        isAmazing = 'True';
+    }
+}
+console.log(`${num} Amazing? ${isAmazing} `);
 }
 
 solve(1233);

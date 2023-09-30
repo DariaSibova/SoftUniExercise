21bce962d249f0eaa81cc57cca815049217ec1c1
Daria Sibova <dariasib05@gmail.com>
2023-09-30 18:32:25 +0300

    commit

---
+++ FUNDAMENTALS/Data Types and Variables Exercise/lower-or-upper.js
@@ -0,0 +1,29 @@
+function solve(char){
+// let code = char.charCodeAt();
+
+// if(code >=65 && code <= 90){
+//     console.log('upper-case')
+// }else {
+//     console.log('lower-case');
+// }
+
+
+//second answer
+// if (char == char.toLowerCase()){
+//     console.log('lower-case');
+// }else {
+//     console.log('upper-case')
+// }
+// }
+
+
+if (char == char.toUpperCase()){
+    //     console.log('upper-case');
+    // }else {
+    //     console.log('lower-case')
+    // }
+     }
+    }
+
+solve('L');
+// solve('f');

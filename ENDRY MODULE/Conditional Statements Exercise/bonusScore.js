9e65ede31beff212f2f45b927f555ea69edbd29e
Daria Sibova <102323456+DariaSibova@users.noreply.github.com>
2023-09-30 14:20:46 +0300

    Update bonusScore.js

--- ENDRY MODULE/Conditional Statements Exercise/bonusScore.js
+++ ENDRY MODULE/Conditional Statements Exercise/bonusScore.js
@@ -1,6 +1,26 @@
-function bonusScore (input){
+function proba(input){
+  let num =Number(input[0]);
+  let bonus = 0;
 
-    let num =Number(input[0]);
+  if (num <= 100){
+
+     bonus = 5;
+  } else if(num > 1000){
+     bonus = num * 0.10;
+  }else {
+     bonus = num * 0.20;
+   
+  }
+
+  if(num % 2 == 0){
+    bonus += 1;
+  }else if (num % 10 == 5){
+    bonus += 2;
+  }
+  console.log(bonus);
+  console.log(num + bonus)
+
+
 }
 
 bonusScore (["20"])

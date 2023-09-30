a0f85c4365b776f55c5c4b94c9f2debe6ea706f1
Daria Sibova <102323456+DariaSibova@users.noreply.github.com>
2023-09-30 14:23:35 +0300

    Update summerOutfit.js

--- ENDRY MODULE/Conditional Statements Advanced  Exercise/summerOutfit.js
+++ ENDRY MODULE/Conditional Statements Advanced  Exercise/summerOutfit.js
@@ -1,8 +1,46 @@
-function summmerOutfit(input){
+function proba(input) {
 
-    
+let gradus = Number(input[0]);
+let day = input[1];
+let outfit ;
+let shoes ;
+
+if(gradus >= 10 && gradus <= 18){
+    if(day == "Morning"){
+        outfit = "Sweatshirt"
+        shoes = "Sneakers"
+    }else if (day == "Afternoon" || day == "Evening"){
+        outfit = "Shirt";
+        shoes = "Moccasins"
+    }
+}else if (gradus > 18 && gradus <= 24){
+    if(day == "Morning" || day == "Evening"){
+        outfit = "Shirt"
+        shoes = "Moccasins"
+    }else if (day == "Afternoon" ){
+        outfit = "T-Shirt";
+        shoes = "Sandals"
+    }
+} else {
+    if(day == "Morning" ){
+        outfit = "T-Shirt"
+        shoes = "Sandals"
+    }else if (day == "Afternoon" ){
+        outfit = "Swim Suit";
+        shoes = "Barefoot"
+    }else {
+        outfit = "Shirt"
+        shoes = "Moccasins"
+    }
+}
+console.log(`It's ${gradus} degrees, get your ${outfit} and ${shoes}.`)
+
+
+
+
+
 
 }
 
 
-summmerOutfit(["16","Morning"])
+proba(["16","Morning"])

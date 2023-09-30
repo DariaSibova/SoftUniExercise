61055633009dfac39b1274330cf22994ec33eda6
Daria Sibova <102323456+DariaSibova@users.noreply.github.com>
2023-09-30 14:25:22 +0300

    Update timeMinutes.js

--- ENDRY MODULE/Conditional Statements Exercise/timeMinutes.js
+++ ENDRY MODULE/Conditional Statements Exercise/timeMinutes.js
@@ -1,5 +1,25 @@
-function timeMinutes (input){
+function proba(input){
+  
+    let hourA = Number(input[0]);
+    let minuteA = Number(input[1]);
+
+    let total = hourA * 60 + minuteA;
+    let extra = total + 15;
+
+    let hour = Math.floor(extra / 60)
+    let minutes = extra % 60;
+
+    if (hour >= 24){
+        hour %= 24;
+       
+    }
+    if(minutes < 10){
+        console.log(`${hour}:0${minutes}`)
+        
+    }else {
+        console.log(`${hour}:${minutes}`)
+    }
+    
 
 }
 
-timeMinutes

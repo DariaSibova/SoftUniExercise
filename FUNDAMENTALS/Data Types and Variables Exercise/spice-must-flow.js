8924b484e7b6422437a07ceae5edefdc2656abed
Daria Sibova <dariasib05@gmail.com>
2023-10-01 14:23:10 +0300

    commit

--- FUNDAMENTALS/Data Types and Variables Exercise/spice-must-flow.js
+++ FUNDAMENTALS/Data Types and Variables Exercise/spice-must-flow.js
@@ -1,4 +1,29 @@
-function solve(){
+function solve(currentYield){
+//initialize result variables
+let spice = 0;
+let days = 0;
+
+//each day check if source is profitable,if so:
+while(currentYield >= 100){
+    //mine yield and add to result
+    // -- reduce yield
+    // -- consume spice
+    days ++;
+    spice += currentYield - 26;
+    currentYield -= 10;
+    //-- otherwise
+    //-- stop mining;
+}
+
+// at end of operation, consume spice, if available
+if (spice >= 26){
+    spice -= 26;
+}
+
+//print result
+console.log(days);
+console.log(spice);
+
 
 }
 

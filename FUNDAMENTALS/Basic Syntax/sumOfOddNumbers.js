df3683a79e050261acc2e04505505e310e8f010a
Daria Sibova <dariasib05@gmail.com>
2023-10-01 14:39:46 +0300

    commit

--- FUNDAMENTALS/Basic Syntax/sumOfOddNumbers.js
+++ FUNDAMENTALS/Basic Syntax/sumOfOddNumbers.js
@@ -1,22 +1,17 @@
 
 function solve(n){
-let totalSum = 0;
+let sum = 0;
+let num = 1;
 
-    for(let i = 1; i <= n;i += 1)
-    if(i % 2 !== 0){
-        console.log(i)
-        
-    }
-    
-    console.log(`Sum: ${totalSum}`);
+while(n > 0){
+    console.log(num);
+    sum += num;
+    num += 2;
+    n--;
 }
 
-
-//Write a program that prints the next n odd numbers (starting from 1) and on the last row prints the sum of them.
-//You will receive a number – n. This number shows how many odd numbers you should print
-//Print the next n odd numbers, starting from 1, separated by newlines.
-
-//On the last line, print the sum of these numbers in the following format: `Sum: {total sum}`
+console.log(`Sum: ${sum}`)
+}
 
 
 solve(5);

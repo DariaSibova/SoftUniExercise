1c0a42c8e841d5b68164df3e47b5a6295ed2b31b
Daria Sibova <dariasib05@gmail.com>
2023-10-01 14:10:49 +0300

    commit

--- ENDRY MODULE/First Steps in Coding More Exercise/weatherForecast.js
+++ ENDRY MODULE/First Steps in Coding More Exercise/weatherForecast.js
@@ -1,3 +1,11 @@
 function solve(input){
-    
+    let whether = input[0];
+    if(whether == "sunny"){
+        console.log("It's warm outside!")
+    }else {
+        console.log("It's cold outside!")
+    }
 }
+solve(["sunny"])
+// solve(["cloudy"])
+// solve(["snowy"])
---
+++ FUNDAMENTALS/Data Types and Variables Exercise/spice-must-flow.js
@@ -0,0 +1,6 @@
+function solve(){
+
+}
+
+solve(111);
+// solve(450)

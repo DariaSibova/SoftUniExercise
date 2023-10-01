8691231e0bc718d9de8e3fe16b41b5ae376c1dd1
Daria Sibova <dariasib05@gmail.com>
2023-10-01 15:04:06 +0300

    commit

---
+++ ENDRY MODULE/Conditional Statements More Exercises/pets.js
@@ -0,0 +1,17 @@
+function solve(input){
+let days = Number(input[0]);
+let food = Number(input[1]);
+let foodPerDayForDog = Number(input[2]);
+let foodPerDayForCat = Number(input[3]);
+let foodPerDayForTurtleInGram = Number(input[4]);
+
+let gramToKilo = foodPerDayForTurtleInGram / 1000;
+let allFoodInKiloForDay = foodPerDayForCat + foodPerDayForDog + gramToKilo ;
+let allFoodInDays = allFoodInKiloForDay * days;
+
+if(food > allFoodInDays ){
+console.log(`${Math.floor(food - allFoodInDays)} kilos of food left`)
+}
+}
+
+solve(["2","10","1","1","1200"])

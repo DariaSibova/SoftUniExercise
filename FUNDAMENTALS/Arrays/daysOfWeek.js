6571216efc40b69f1b72e8352e796337a04ed5b5
Daria Sibova <dariasib05@gmail.com>
2023-10-02 17:50:24 +0300

    commit

--- FUNDAMENTALS/Arrays/daysOfWeek.js
+++ FUNDAMENTALS/Arrays/daysOfWeek.js
@@ -1,5 +1,8 @@
 function solve(index){
-    let days = [
+    // if(index < 1 || index > 7){
+    //     console.log('Invalid day!');
+    // }else{  
+        let days = [
         'Monday',
         'Tuesday',
         'Wednesday',
@@ -8,7 +11,21 @@ function solve(index){
         'Saturday',
         'Sunday'
     ];
-    console.log(days[index - 1]);
-
+    // console.log(days[index - 1]);
+    let result = days[index - 1];
+    if(result != undefined){
+        console.log(result)
+    }else {
+        console.log(`Invalid day!`);
+    }
 }
+
+// let days = [ 'Monday','Tuesday','Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+// if(index >= 1 && index <=7){
+//     //todo
+// }else {
+//     //todo
+// }
+  
+// }
 solve(3);

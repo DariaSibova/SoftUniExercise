3e76ff2f1a3f03ca23df81787ea13adb72d80d80
Daria Sibova <dariasib05@gmail.com>
2023-10-02 15:46:01 +0300

    commit

---
+++ FUNDAMENTALS/Arrays/daysOfWeek.js
@@ -0,0 +1,14 @@
+function solve(index){
+    let days = [
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+        'Sunday'
+    ];
+    console.log(days[index - 1]);
+
+}
+solve(3);
--- FUNDAMENTALS/Arrays/sum-first-and-last-array-elements.js
+++ FUNDAMENTALS/Arrays/sum-first-and-last-array-elements.js
@@ -1,4 +1,7 @@
 function solve(nums){
+    // let first = nums[0];
+    // let last = nums[nums.length - 1];
+    // console.log(first + last)
     console.log(nums[0] + nums[nums.length - 1]);
 }
 

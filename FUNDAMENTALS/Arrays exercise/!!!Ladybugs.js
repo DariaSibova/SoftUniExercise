5d3cca64479b7d9fce991efa8215661b8cfeaf7f
Daria Sibova <dariasib05@gmail.com>
2023-10-12 17:34:36 +0300

    commit

---
+++ FUNDAMENTALS/Arrays exercise/!!!Ladybugs.js
@@ -0,0 +1,6 @@
+// Given field size | size 3 -> [-,-,-]
+function solve(arr){
+
+}
+
+solve([3,'0 1','0 right 1','2 right 1']);
---
+++ FUNDAMENTALS/Bitwise Operations/bit-destroyer.js
@@ -0,0 +1,8 @@
+function solve(number,position){
+    let mask = 1 << position;
+    mask = ~mask;
+    let result = number & mask;
+    console.log(result)
+}
+
+solve(1313,5)

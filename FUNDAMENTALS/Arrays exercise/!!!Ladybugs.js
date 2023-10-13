1ee8f7f10ce4516b5a74e71fa9edb0405870e7c3
Daria Sibova <dariasib05@gmail.com>
2023-10-13 18:05:31 +0300

    commit

--- FUNDAMENTALS/Arrays exercise/!!!Ladybugs.js
+++ FUNDAMENTALS/Arrays exercise/!!!Ladybugs.js
@@ -1,6 +1,12 @@
-// Given field size | size 3 -> [-,-,-]
+// Given field size | size 3 -> [-,-,-] | size 5 [-,-,-,-,-]
+// No ledybug -> 0 | ladybug -> 1
+// Given initial ladybugs indeces -> size 3, indeces '0 2' -> [1,0,1]
+// Commands -> "{ladybug index} {direction} {fly length}" -> "0 right 2"
+// If ladybug lands an another -> continues in same direction with same length
 function solve(arr){
 
 }
 
 solve([3,'0 1','0 right 1','2 right 1']);
+// solve([ 3, '0 1 2', '0 right 1', '1 right 1', '2 right 1'])
+// solve([ 5, '3', '3 left 2', '1 left -2'])

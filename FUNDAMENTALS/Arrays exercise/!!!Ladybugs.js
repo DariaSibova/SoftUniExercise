a88176245499bd4c6bb482dd391f0f2b535840a0
Daria Sibova <dariasib05@gmail.com>
2023-10-13 18:30:42 +0300

    commit

--- FUNDAMENTALS/Arrays exercise/!!!Ladybugs.js
+++ FUNDAMENTALS/Arrays exercise/!!!Ladybugs.js
@@ -11,21 +11,35 @@ function solve(arr) {
   let ladybugIdxs = arr[1].split(" ").map(Number);
   let field = [];
 
-  for (let i = 0; i < fieldSize; i++){
-    if(ladybugIdxs.includes(i)){
+  for (let i = 0; i < fieldSize; i++) {
+    if (ladybugIdxs.includes(i)) {
       field[i] = 1;
     } else {
       field[i] = 0;
     }
   }
-  
-  for ( let j = 2; j < arr.length; j++){
-    let command = arr[j];
-    let tokens = command.split(' ');
 
-    let ledybugIdx = Number(tokens[0]);
+  for (let j = 2; j < arr.length; j++) {
+    let command = arr[j];
+    let tokens = command.split(" ");
+
+    let ladybugIdx = Number(tokens[0]);
     let direction = tokens[1];
     let flyLength = Number(tokens[2]);
+
+    if (!field[ladybugIdx]) {
+      continue;
+    }
+
+    field[ladybugIdx] = 0;
+
+    if (direction == "left") {
+      let newIdx = ladybugIdx - flyLength;
+
+      if (newIdx >= 0) {
+        field[newIdx] = 1;
+      }
+    }
   }
 }
 

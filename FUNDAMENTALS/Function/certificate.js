a6ed298dd45d630cea1836d4ebf44d98a7210e91
Daria Sibova <dariasib05@gmail.com>
2023-10-17 13:31:48 +0300

    commit

--- FUNDAMENTALS/Function/certificate.js
+++ FUNDAMENTALS/Function/certificate.js
@@ -1,16 +1,43 @@
-function printHeader(){
-    console.log('~~~- {@} -~~~') ;
-    console.log('~- Certificate -~');
-    console.log(' ~~~- ~---~ -~~~')
-}
-function printName(nameArr){
-    console.log(nameArr[0] + ' ' + nameArr[1])
-}
-function printGrade(grade){
-console.log(grade[0] + ' ' + grade[1]);
+function printCertificate(grade, names) {
+  if (pass(grade)) {
+    printHeader();
+    printName(names);
+    formatGrade(grade);
+  } else {
+    console.log("Student does not qualify");
+  }
 }
 
+function printHeader() {
+  console.log("~~~- {@} -~~~");
+  console.log("~- Certificate -~");
+  console.log(" ~~~- ~---~ -~~~");
+}
+function printName(nameArr) {
+  console.log(nameArr[0] + " " + nameArr[1]);
+}
+function pass(grade) {
+  return grade >= 3;
+}
+function formatGrade(grade) {
+  let label;
 
-printHeader()
-printName(['John','Smith']);
-printGrade(['Very good' ,'(5.25)'])
+  if (grade < 3) {
+    label = "Fail";
+    grade = 2;
+  } else {
+    if (grade < 3.5) {
+      label = "Poor";
+    } else if (grade < 4.5) {
+      label = "Good";
+    } else if (grade < 5.5) {
+      label = "Very good";
+    } else {
+      label = "Excellent";
+    }
+    grade = grade.toFixed(2);
+  }
+  console.log(`${label} (${grade})`);
+}
+printCertificate(5.25, ["John", "Smith"]);
+printCertificate(2, ["Peter", "Johnson"]);

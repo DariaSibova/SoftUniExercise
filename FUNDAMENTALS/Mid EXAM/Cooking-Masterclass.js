f5d0159cd79bc702758bf7c53f4ecae24c4f2373
Daria Sibova <dariasib05@gmail.com>
2023-10-22 14:17:20 +0300

    commit

--- FUNDAMENTALS/Mid EXAM/Cooking-Masterclass.js
+++ FUNDAMENTALS/Mid EXAM/Cooking-Masterclass.js
@@ -10,32 +10,26 @@
 // ${neededMoney}$ more needed
 
 
-function cookingMasterclass(input){
-let budget = input[0];
-let students = input[1];
-let flourPrice = input[2];
-let eggPrice = input[3];
-let apronPrice = input[4];
+function cookingMasterclass(input) {
+    let budget = input[0];
+    let students = input[1];
+    let flourPrice = input[2];
+    let eggPrice = input[3];
+    let apronPrice = input[4];
+  
+    let priceForApron = Math.ceil(students * 1.2); 
+  
+    let freePac = Math.floor(students / 5); 
+  
+    let totalCost = (apronPrice * priceForApron) + (eggPrice * 10 * students) + (flourPrice * (students - freePac));
+  
+    if (totalCost <= budget) {
+      console.log(`Items purchased for ${totalCost.toFixed(2)}$.`);
+    } else if (totalCost > budget) {
+      console.log(`${(totalCost - budget).toFixed(2)}$ more needed.`);
+    }
+  }
 
-let priceForApron = students + 0.20;
-let priceForApronMath = (Math.ceil(priceForApron))
-
-
-let freePac = 0;
-freePac = Math.floor((students / 5));
-
-
-
-let priceForOnePerson = (apronPrice * priceForApronMath) + (eggPrice * 10 * students) + flourPrice * (students - freePac);
-
-
-if (priceForOnePerson <= budget){
-    console.log(`Items purchased for ${priceForOnePerson.toFixed(2)}$.`);
-} else if(priceForOnePerson > budget){
-    console.log(`${(priceForOnePerson - budget).toFixed(2)}$ more needed.`)
-}
-}
-
-// cookingMasterclass([50,2,1.0,0.10,10.0]); // Items purchased for 34.00$.
-// cookingMasterclass([100,25,4.0,1.0,6.0]) // 410.00$ more needed.
-cookingMasterclass([946,20,12.05,0.42,27.89]) // 0.16$ more needed.
+cookingMasterclass([50,2,1.0,0.10,10.0]); // Items purchased for 34.00$.
+cookingMasterclass([100,25,4.0,1.0,6.0]) // 410.00$ more needed.
+// cookingMasterclass([946,20,12.05,0.42,27.89]) // 0.16$ more needed.

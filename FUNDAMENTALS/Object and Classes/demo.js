c3559501115eb8b53c8e00c40f4dd2051d6e8cc6
Daria Sibova <dariasib05@gmail.com>
2023-11-04 13:29:39 +0200

    commit

--- FUNDAMENTALS/Object and Classes/demo.js
+++ FUNDAMENTALS/Object and Classes/demo.js
@@ -1,11 +1,23 @@
+// let peter = {
+//     name: 'Peter',
+//     age: 20,
+//     jeight: 183
+// };
+// printDetails(peter);
+
+// function printDetails(person){
+//     // person.age = 21;
+//     console.log(peter.age);
+// }
+
+//-----------------------------------
+
 let peter = {
     name: 'Peter',
     age: 20,
     jeight: 183
 };
-printDetails(peter);
-
-function printDetails(person){
-    // person.age = 21;
-    console.log(peter.age);
-}
+console.log(peter);
+peter.hairColor = 'brown';
+console.log(peter);
+console.log(peter['name']);

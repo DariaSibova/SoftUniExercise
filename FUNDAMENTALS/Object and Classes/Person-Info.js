b169b232cdc50a620d94f93fac83a955fa8b3454
Daria Sibova <dariasib05@gmail.com>
2023-11-04 13:30:11 +0200

    create person Info

---
+++ FUNDAMENTALS/Object and Classes/Person-Info.js
@@ -0,0 +1,17 @@
+function solve(firstName,lastName,age){
+    let person = {};
+    person.firstName = firstName;
+    person.lastName = lastName;
+    person.age = age;
+
+    return person;
+}
+let result = solve('Peter','Pan',20);
+console.log(result)
+
+
+solve("Peter",
+
+"Pan",
+
+"20")

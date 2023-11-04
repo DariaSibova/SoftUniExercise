f739694d71e2002637cdcb89c153ded168edd7f4
Daria Sibova <dariasib05@gmail.com>
2023-11-04 13:50:33 +0200

    commit

--- FUNDAMENTALS/Object and Classes/Person-Info.js
+++ FUNDAMENTALS/Object and Classes/Person-Info.js
@@ -1,8 +1,19 @@
 function solve(firstName,lastName,age){
-    let person = {};
-    person.firstName = firstName;
-    person.lastName = lastName;
-    person.age = age;
+    // let person = {};
+    // person.firstName = firstName;
+    // person.lastName = lastName;
+    // person.age = age;
+    //--------------------------
+    // let person = {};
+    // person['firstName'] = firstName;
+    // person['lastName'] = lastName;
+    // person['age'] = age;
+    //------------------------------
+    let person = {
+        firstName,
+        lastName,
+        age
+    };
 
     return person;
 }

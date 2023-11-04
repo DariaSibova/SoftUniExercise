21dd79f8b89edc6b6d670383b8b59b666baa4fac
Daria Sibova <dariasib05@gmail.com>
2023-11-04 13:26:27 +0200

    commit

---
+++ FUNDAMENTALS/Object and Classes/demo.js
@@ -0,0 +1,11 @@
+let peter = {
+    name: 'Peter',
+    age: 20,
+    jeight: 183
+};
+printDetails(peter);
+
+function printDetails(person){
+    // person.age = 21;
+    console.log(peter.age);
+}

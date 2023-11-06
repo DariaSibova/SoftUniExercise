2d6f2ecada4a1b70999ff5c3e335a8efc12fbc32
Daria Sibova <dariasib05@gmail.com>
2023-11-06 12:51:04 +0200

    commit

--- FUNDAMENTALS/Object and Classes/demo.js
+++ FUNDAMENTALS/Object and Classes/demo.js
@@ -35,15 +35,49 @@
 // peter.name;
 // peter.sayHello()
 // -----------------------------------
-let peter = {
-    name: 'Peter',
-     age: 20,
-    height: 183
+// let peter = {
+//     name: 'Peter',
+//      age: 20,
+//     height: 183
  
-    };
-    peter.sayHello = function(){
-        console.log('Hello!');
-    };
-    peter.sayHello();
+//     };
+//     peter.sayHello = function(){
+//         console.log('Hello!');
+//     };
+//     peter.sayHello();
+    //-----------------------------
+    // let peter = {
+    //     name: 'Peter',
+    //      age: 20,
+    //     height: 183,
+    //     sayHello: () =>{
+    //         console.log('Hello!');
+    //     }
+     
+    //     };
+    //     peter.sayHello()
+    //------------------------------
+    let peter = {
+        name: 'Peter',
+         age: 20,
+        height: 183,
+        sayHello(){
+            console.log('Hello!');
+        }
+     
+        };
+        // peter.sayHello()  
+        let keys = Object.keys(peter);
+        // console.log(keys)
+        // console.log(keys[0]) 
+        console.log(peter[keys[0]]) 
+        let values = Object.values(peter);
+        console.log(values);
+        let entries = Object.entries(peter);
+        // console.log(entries)
+        // console.log(entries[0])
+        console.log(entries[0][1])
 
-
+        for (let key of Object.keys(peter)){
+            console.log(key, '->',peter[key])
+        }

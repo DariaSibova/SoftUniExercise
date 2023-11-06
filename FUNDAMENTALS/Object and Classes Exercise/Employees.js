517eba0ff2936110b6d275fe85661891b8ef5466
Daria Sibova <dariasib05@gmail.com>
2023-11-06 15:46:38 +0200

    commit

--- FUNDAMENTALS/Object and Classes Exercise/Employees.js
+++ FUNDAMENTALS/Object and Classes Exercise/Employees.js
@@ -1,8 +1,8 @@
 function employees(arr){
-    // for(let employeeName of arr){
-for(let name of arr){
-        // let employee = {name: employeeName, personalNum: employeeName.length};
-        let employee = {name , personalNum: name.langth};
+    for(let employeeName of arr){
+// for(let name of arr){
+        let employee = {name: employeeName, personalNum: employeeName.length};
+        // let employee = {name , personalNum: name.langth};
         console.log(`Name: ${employee.name} -- Personal Number: ${employee.personalNum}`);
     }
 }

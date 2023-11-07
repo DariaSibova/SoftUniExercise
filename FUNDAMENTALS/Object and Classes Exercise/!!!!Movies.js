0448b61956f9adf6f0f4a834babf4d8a2380a684
Daria Sibova <dariasib05@gmail.com>
2023-11-07 15:21:20 +0200

    commit

---
+++ FUNDAMENTALS/Object and Classes Exercise/!!!!Movies.js
@@ -0,0 +1,30 @@
+function manageMovie(arr){
+    let movies = [];
+
+
+    for(let command of arr){
+
+    }
+
+}
+
+
+manageMovie([
+    'addMovie Fast and Furious',
+    'addMovie Godfather',
+    'Inception directedBy Christopher Nolan',
+    'Godfather directedBy Francis FordCoppola',
+     'Godfather onDate 29.07.2018',
+    'Fast and Furious onDate 30.07.2018',
+    'Batman onDate 01.08.2018',
+    'Fast and Furious directedBy Rob Cohen'])
+
+
+    // manageMovie([ 
+        // 'addMovie The Avengers',
+    //     'addMovie Superman',
+    //     'The Avengers directedBy Anthony Russo',
+    //     'The Avengers onDate 30.07.2010',
+    //     'Captain America onDate 30.07.2010',
+    //     'Captain America directedBy Joe Russo'
+    //     ])

3af1e686ab6485405603bfc4c5ea9a4db2195c0a
Daria Sibova <dariasib05@gmail.com>
2023-11-07 15:27:44 +0200

    commit

--- FUNDAMENTALS/Object and Classes Exercise/Class-Storage.js
+++ FUNDAMENTALS/Object and Classes Exercise/Class-Storage.js
@@ -1,17 +1,27 @@
-function solve(){
+class storage{
+    constructor(capacity){
+        this.capacity = capacity;
+        this.storage = [];
+        this.totalCost = 0;
+    }
 
+addProduct(product){
+    this.storage.push(product);
+    this.capacity -= product.quantity;
+    this.totalCost += product.price * product.quantity;
 
+}
+getProducts(){
+    return this.storage.map(product => JSON.stringify(product)).join('\n');
+}
 }
 
 
 
 
-let productOne = {name: 'Cucamber',
-price: 1.50, quantity: 15};
-let productTwo = {name: 'Tomato',
-price: 0.90, quantity: 25};
-let productThree = {name: 'Bread',
-price: 1.10, quantity: 8};
+let productOne = {name: 'Cucamber',price: 1.50, quantity: 15};
+let productTwo = {name: 'Tomato',price: 0.90, quantity: 25};
+let productThree = {name: 'Bread',price: 1.10, quantity: 8};
 let storage = new Storage(50);
 storage.addProduct(productOne);
 storage.addProduct(productTwo);

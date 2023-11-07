815ede9b467579980d2e8c93fa10fe0f4719ed73
Daria Sibova <dariasib05@gmail.com>
2023-11-07 15:27:20 +0200

    commit

--- FUNDAMENTALS/Object and Classes Exercise/!!!!Movies.js
+++ FUNDAMENTALS/Object and Classes Exercise/!!!!Movies.js
@@ -3,9 +3,32 @@ function manageMovie(arr){
 
 
     for(let command of arr){
+if(command.includes('addMovie')){
+    let movieName = command.split('addMovie'[1]);
+    let movieObj = {name: movieName};
+    movies.push(movieObj);
 
+} else if(command.includes('directedBy')){
+    let [movieName,directorName] = command.split('directedBy');
+
+    let movie = movies.find(movie => movie.name == movieName);
+    if(movie){
+        movie.director =directorName;
     }
+} else if(command.includes('onDate')){
+let [movieName,date] = command.split(' onDate ');
+let movie = movies.find(movie => movie.name == movieName);
 
+if(movie){
+    movie.date = date;
+}
+}
+    }
+ for (let movie of movies){
+    if(movie.name && movie.director && movie.date){
+        console.log(JSON.stringify(movie));
+    }
+ }
 }
 
 

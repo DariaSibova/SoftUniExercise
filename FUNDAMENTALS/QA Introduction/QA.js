702cb782b719bd136e23106e6f0e83fffc7118b3
Daria Sibova <dariasib05@gmail.com>
2023-11-09 17:28:40 +0200

    commit

---
+++ FUNDAMENTALS/QA Introduction/QA.js
@@ -0,0 +1,5 @@
+// • Software Quality Assurance;
+// • QA Engineers and Resposibilities;
+// • Bugs and Bug Trackers;
+// • Testing, Test Types and Test Levels;
+// • Test Automation, Frameworks and Tools.

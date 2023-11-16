ecfd91c70879eeaf1d70a79a4c6e18b98198bdc0
Daria Sibova <dariasib05@gmail.com>
2023-11-16 14:56:07 +0200

    commit

---
+++ FUNDAMENTALS/Associative Arrays/demo2.js
@@ -0,0 +1,11 @@
+let myObj ={
+    name: 'Peter'
+};
+
+console.log(Object.hasOwn(myObj, 'name'));
+console.log(Object.hasOwn(myObj, 'age'));
+
+console.log(Object.hasOwnProperty('name'));
+console.log(Object.hasOwnProperty('age'));
+
+

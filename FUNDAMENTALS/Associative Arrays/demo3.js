733b671e089aae9667c8b289e23649bb55e439c0
Daria Sibova <dariasib05@gmail.com>
2023-11-16 17:46:34 +0200

    commit

---
+++ FUNDAMENTALS/Associative Arrays/demo3.js
@@ -0,0 +1,31 @@
+let mySet = new Set();
+
+// console.log(mySet);
+
+// mySet.add(5);
+
+console.log(mySet)
+
+mySet.add(3);
+mySet.add(5);
+console.log(mySet);
+
+// for(let entry of mySet){
+// console.log(entry);
+// }
+
+for(let entry of mySet.keys()){
+    console.log(entry);
+    }
+
+    for(let entry of mySet.entries()){
+        console.log(entry);
+        }
+    
+
+// // mySet.delete(3);
+// console.log(mySet.delete(3));
+
+// console.log(mySet)
+// // mySet.delete(3);
+// console.log(mySet.delete(3));

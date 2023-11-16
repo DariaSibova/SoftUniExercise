4c504bc02d1f568c19cf3bf4628168e0b6f19499
Daria Sibova <dariasib05@gmail.com>
2023-11-16 17:35:06 +0200

    commit

---
+++ FUNDAMENTALS/Associative Arrays/!!!School-Grades.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+solve(['Lilly 4 6 6 5',
+
+'Tim 5 6',
+
+'Tammy 2 4 3',
+
+'Tim 6 6'])
+
+solve(['Steven 3 5 6 4',
+
+'George 4 6',
+
+'Tammy 2 5 3',
+
+'Steven 6 3'])
---
+++ FUNDAMENTALS/Associative Arrays/!!!Word-Occurrences.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+
+
+
+
+solve (["Here", "is", "the", "first", "sentence",
+
+"Here", "is", "another", "sentence", "And",
+
+"finally", "the", "third", "sentence"])
+
+solve(["dog", "bye", "city", "dog", "dad",
+
+"boys", "ginger"])

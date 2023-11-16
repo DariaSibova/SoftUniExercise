bc691f80919086b86ce350f5636ce2f4fb1ffc7a
Daria Sibova <dariasib05@gmail.com>
2023-11-16 15:15:38 +0200

    commit

--- FUNDAMENTALS/Associative Arrays/phone-book.js
+++ FUNDAMENTALS/Associative Arrays/phone-book.js
@@ -1,34 +1,57 @@
-function solve(input){
-    let phoneBook = {};
+// let solve = (input) => Object.entries(Object.fromEntries(input.map(item => item.split(''))))
+// .map(entry => entry.join(' -> '))
+// .join('\n')
 
-    for(let item of input){
-        let [name, phone] = item.split(' ');
+// let result = solve([
+//     "Tim 0834212554",
+  
+//     "Peter 0877547887",
+  
+//     "Bill 0896543112",
+  
+//     "Tim 0876566344",
+//   ]);
 
-        phoneBook[name] = phone;
-    }
-let output = Object.entries(phoneBook)
-.map(entry => entry.join(' -> '))
-.join('\n');
-console.log(output);
-    // for(let entry of Object.entries(phoneBook)){
-    //     console.log(entry.join( ' -> '));
-    // }
-    // console.log(phoneBook);
+
+function solve(input) {
+  let phoneBook = {};
+
+  for (let item of input) {
+    let [name, phone] = item.split(" ");
+
+    phoneBook[name] = phone;
+  }
+  let output = Object.entries(phoneBook)
+    .map((entry) => entry.join(" -> "))
+    .join("\n");
+  console.log(output);
+
+  // for(let entry of Object.entries(phoneBook)){
+  //     console.log(entry.join( ' -> '));
+  // }
 }
+//-------------------------------------------------
+// function solve(input){
+//     let phoneBook = {}
+//     for(let item of input){
+//         let tokens = item.split(' ');
+//         console.log(tokens);
+//         let [name,phone] = tokens;
 
-solve(['Tim 0834212554',
+//         phoneBook[name] = phone;
+//     }
 
-'Peter 0877547887',
+//     console.log(Object.entries(phoneBook));
+// }
 
-'Bill 0896543112',
+solve([
+  "Tim 0834212554",
 
-'Tim 0876566344'])
+  "Peter 0877547887",
 
+  "Bill 0896543112",
 
-solve(['George 0552554',
+  "Tim 0876566344",
+]);
 
-'Peter 087587',
-
-'George 0453112',
-
-'Bill 0845344'])
+solve(["George 0552554", "Peter 087587", "George 0453112", "Bill 0845344"]);

f3630b02ea126825dd09f0291e9e8e63b459b6c4
Daria Sibova <dariasib05@gmail.com>
2023-11-17 16:04:25 +0200

    commit

---
+++ FUNDAMENTALS/Associative Arrays Exercise/!!!Arena-Tier.js
@@ -0,0 +1,27 @@
+
+
+
+
+solve ([
+
+    'Peter -> BattleCry -> 400',
+    
+    'Alex -> PowerPunch -> 300',
+    
+    'Stefan -> Duck -> 200',
+    
+    'Stefan -> Tiger -> 250',
+    
+    'Ave Cesar'
+    
+    ])
+
+    // solve([ 'Peter -> Duck -> 400', 
+    // 'Julius -> Shield -> 150', 
+    // 'Gladius -> Heal -> 200',
+    //  'Gladius -> Support -> 250', 
+    //  'Gladius -> Shield -> 250', 
+    //  'Peter vs Gladius',
+    //  'Gladius vs Julius',
+    //  'Gladius vs Maximilian', 
+    //  'Ave Cesar' ])

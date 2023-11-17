c5982d23d9e0b4d13cfacb54ee7923245db02daa
Daria Sibova <dariasib05@gmail.com>
2023-11-17 16:02:45 +0200

    commit

---
+++ FUNDAMENTALS/Associative Arrays Exercise/!!!Travel-Time.js
@@ -0,0 +1,32 @@
+ 
+
+
+
+
+ solve ([
+
+    "Bulgaria > Sofia > 500",
+    
+    "Bulgaria > Sopot > 800",
+    
+    "France > Paris > 2000",
+    
+    "Albania > Tirana > 1000",
+    
+    "Bulgaria > Sofia > 200"
+    
+    ])
+
+    // solve([
+
+    //     'Bulgaria > Sofia > 25000',
+        
+    //     'Bulgaria > Sofia > 25000',
+        
+    //     'Kalimdor > Orgrimar > 25000',
+        
+    //     'Albania > Tirana > 25000',
+        
+    //     'Bulgaria > Varna > 25010',
+        
+    //     'Bulgaria > Lukovit > 10'])

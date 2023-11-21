4a2b002a84d4e263678d8fb09f7dbb7524b81dfc
Daria Sibova <dariasib05@gmail.com>
2023-11-21 18:43:40 +0200

    commit

--- FUNDAMENTALS/Object and Classes Exercise/Class-Storage.js
+++ FUNDAMENTALS/Object and Classes Exercise/Class-Storage.js
@@ -16,9 +16,6 @@ getProducts(){
 }
 }
 
-
-
-
 let productOne = {name: 'Cucamber',price: 1.50, quantity: 15};
 let productTwo = {name: 'Tomato',price: 0.90, quantity: 25};
 let productThree = {name: 'Bread',price: 1.10, quantity: 8};

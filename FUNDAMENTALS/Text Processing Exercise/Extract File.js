d503c1e0a47af097f11e67f3a8b22c4f466b579a
Daria Sibova <dariasib05@gmail.com>
2023-11-22 18:25:08 +0200

    commit

---
+++ FUNDAMENTALS/Text Processing Exercise/Extract File.js
@@ -0,0 +1,13 @@
+function solve(path){
+    let fileInfo = path.split('\\').pop();
+    let lastDotIdx = fileInfo.lastIndexOf('.');
+
+    let fileName = fileInfo.split(0, lastDotIdx);
+    let fileExtension = fileInfo.slice(lastDotIdx + 1);
+
+    console.log(`File name: ${fileName}`);
+    console.log(`File extension: ${fileExtension}}`)
+
+}
+solve('C:\\Internal\\training-internal\\Template.pptx')
+// solve('C:\\Projects\\Data-Structures\\LinkedList.cs')

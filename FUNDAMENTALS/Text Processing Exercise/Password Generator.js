59662255ed11439fc84167788ff64e4fd8d2ba68
Daria Sibova <dariasib05@gmail.com>
2023-11-22 19:29:13 +0200

    commit

--- FUNDAMENTALS/Text Processing Exercise/Password Generator.js
+++ FUNDAMENTALS/Text Processing Exercise/Password Generator.js
@@ -2,13 +2,13 @@ function solve(arr){
     let [str1,str2,replacementStr] = arr;
     let concatedStr = str1 + str2 ;
 
-    // let vowels = ['a','e','i','o','u'];
+    let vowels = ['a','e','i','o','u'];
 
     let idx = 0;
 
     for (let char of concatedStr){
-        // if(vowels.includes(char)){
-        if(/[aeoui]/.includes(char)){
+        if(vowels.includes(char)){
+        // if(/[aeoui]/.includes(char)){
 
             concatedStr = concatedStr.replace(char,replacementStr[idx].toUpperCase());
             idx++;

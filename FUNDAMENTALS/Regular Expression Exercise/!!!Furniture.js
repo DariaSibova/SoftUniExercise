bd2738707b8d967a461dc66974bbf4656f9f95a5
Daria Sibova <dariasib05@gmail.com>
2023-11-28 16:42:45 +0200

    commit

---
+++ FUNDAMENTALS/Regular Expression Exercise/!!!Furniture.js
@@ -0,0 +1,28 @@
+function solve ( arr){
+    let items = [];
+    let totalPrice = 0;
+    let pattern = />>(?<name>[A-Z][A-Za-z]+)<<(?<price>\d+\.?\d*)!(?<qty>\d+)/;
+    let command = arr.shift();
+
+    while (command != 'Purchase'){
+        let match = command.match(pattern);
+        
+        if (match){
+           // let name = match.groups.name;
+           // let price = number (match.groups.price);
+           // let qty = Number(match.groups.qty);
+           let {name,place,qty} = match.groups;
+           let furniturePrice = Number(qty) * Number(price);
+           items.push(name);
+           totalPrice += furniturePrice;
+        }
+        command = arr.shift()
+    }
+    console.log('Bought furniture:');
+    if(items.length > 0){
+        console.log(items.join('\n'));
+
+    }
+    console.log(`Total money spend: ${totalPrice.toFixed(2)}`);
+}
+solve()

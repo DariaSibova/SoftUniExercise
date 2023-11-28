2478b18d1e2b973d1ebfea5f463c4d3da484a82b
Daria Sibova <dariasib05@gmail.com>
2023-11-28 17:33:38 +0200

    commit

---
+++ FUNDAMENTALS/Function/01.function.js
@@ -0,0 +1,6 @@
+// • What is a Function?;
+// • Declaring and Invoking Functions;
+// • Nested Functions;
+// • Value vs Reference Types;
+// • Arrow Functions;
+// • Naming and Best Practices.
---
+++ FUNDAMENTALS/HTML and CSS/html.js
@@ -0,0 +1,7 @@
+// Intro to HTML;
+// • HTML Common Elements;
+// • Introduction to CSS;
+// • Selectors;
+// • Block Elements;
+// • Dev Tools;
+// • Fonts.

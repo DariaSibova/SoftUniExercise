d3dbbde176779d5fe13f7eb653f43f833f81f846
Daria Sibova <dariasib05@gmail.com>
2023-12-01 16:13:42 +0200

    commit

---
+++ FUNDAMENTALS/exam preparation final/!!Mirror Words.js
@@ -0,0 +1,43 @@
+function solve([input]){
+    // compose regexp
+    // find all matches in a loop 
+    // for every match:
+    // - check if second word reversed is the same as the first
+
+    // print result
+    let result = [];
+    let count = 0;
+
+    let pattern = /(#|@)([A-Za-z]{3,})\1\1([A-Za-z]{3,})\1/gi;
+
+    let match = pattern.exec(input);
+
+    while(match){
+        count++
+       let first = match[2];
+       let second = match[3];
+       if(first == second.split('').reverse().join('')){
+        result.push(`${first} <=> ${second}`);
+       }
+        match = pattern.exec(input);
+    }
+    if(count == 0){
+        console.log('No word pairs found!');
+
+    } else {
+        console.log(`${count} word pairs found!`);
+    }
+
+    if(result.length == 0){
+        console.log('No mirror words!');
+    } else {
+        console.log('The mirror words are:');
+        console.log(result.join(', '));
+    }
+    console.log(result)
+}
+
+solve([
+    '@mix#tix3dj#poOl##loOp#wl@@bong&song%4very$long@thong#Part##traP##@@leveL@@Level@##car#rac##tu@pack@@ckap@#rr#sAw##wAs#r#@w1r'
+  ]
+  )

a324b921dfb9dea08475d4ecfaba271ee11d0fd4
Daria Sibova <dariasib05@gmail.com>
2024-01-17 13:56:58 +0200

    commit

--- ADVANCED/Syntax, Functions and Statements/String Length.js
+++ ADVANCED/Syntax, Functions and Statements/String Length.js
@@ -7,5 +7,16 @@ function solve(a,b,c){
     console.log(avg);
 }
 
+// function solve(str1, str2, str3) {
+//     let len1 = str1.length;
+//     let len2 = str2.length;
+//     let len3 = str3.length;
+//     let sumLength = len1 + len2 + len3;
+//     let averageLength = Math.floor(sumLength / 3);
+//     console.log(sumLength);
+//     console.log(averageLength);
+//     }
+
+
 solve('chocolate', 'ice cream', 'cake');
 solve('pasta', '5', '22.3');

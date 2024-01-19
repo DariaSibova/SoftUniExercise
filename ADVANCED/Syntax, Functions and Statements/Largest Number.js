27b108c83acef6cbc5a0fb4c5bc6d91a2549f0da
Daria Sibova <dariasib05@gmail.com>
2024-01-19 13:04:56 +0200

    commit

--- ADVANCED/Syntax, Functions and Statements/Largest Number.js
+++ ADVANCED/Syntax, Functions and Statements/Largest Number.js
@@ -20,6 +20,14 @@ function solve(a,b,c){
 //     console.log(`The largest number is ${Math.max(...params)}.`);
 //     }
     
+// function solv(...params){
+//     const largest = Math.max(...params);
+
+//     const myObj = {a : 5, name: 'peter'};
+//     const{...copy} = myObj;
+
+//     console.log(largest)
+// }
 
 solve(5, -3, 16);
 solve(-3, -5, -22.5);

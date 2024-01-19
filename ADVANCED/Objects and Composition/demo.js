ab41b3bcb21f743a7ef4d678c43abd9a4e7bac07
Daria Sibova <dariasib05@gmail.com>
2024-01-19 15:06:56 +0200

    commit

--- ADVANCED/Objects and Composition/demo.js
+++ ADVANCED/Objects and Composition/demo.js
@@ -2,3 +2,17 @@ const arr = [10,20,30,40];
 
 console.log(arr);
 console.log(arr[1]);
+const myArr = [15,16,17];
+// const result = arr.splice(1,2,15,16,17);
+const result = arr.splice(1,0, ...myArr)
+
+console.log(arr);
+console.log(result);
+console.log("---------------")
+
+const arr1 = [3,7,1,5,4,12];
+console.log(arr1);
+arr1.sort(
+    
+)
+console.log(arr1)

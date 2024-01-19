0ded62a74b285fa78608f300ab056c9af935a0c8
Daria Sibova <dariasib05@gmail.com>
2024-01-19 14:23:28 +0200

    commit

---
+++ ADVANCED/Objects and Composition/Diagonal Sums.js
@@ -0,0 +1,11 @@
+function solve(matrix){
+    let main = 0;
+    let secondary = 0;
+
+    for(let i = 0; i < matrix.length;i++){
+        main += matrix[i][i];
+        secondary += matrix[i][matrix.length - 1 -i]
+    }
+
+    console.log(main, secondary);
+}

486e39dfefc7f3f0d393d64b5d969c1f3df6670d
Daria Sibova <dariasib05@gmail.com>
2024-01-19 14:23:16 +0200

    commit

---
+++ ADVANCED/Objects and Composition/Process Odd Positions.js
@@ -0,0 +1,19 @@
+function solve(arr) {
+    return arr.filter((a, i) => i % 2 !== 0)
+    .map(x => x * 2)
+    .reverse()
+    .join(' ');
+    }
+
+//     // return  arr.filter((c,i) => i % 2).map(e => e * 2).reverse().jpin(' ');
+//     // console.log(filtered);
+//     // const doubled = filtered.map(e => e * 2);
+//     // console.log(doubled)
+//     // doubled.reverse();
+//     // console.log(doubled);
+//     // const result = doubled.join(' ');
+//     // console.log(result)
+
+
+console.log(solve([10, 15, 20, 25]))
+solve([3, 0, 10, 4, 7, 3])

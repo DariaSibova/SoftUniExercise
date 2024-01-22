d72ed28350bb9ffda73c77e6d512566d11181ee4
Daria Sibova <dariasib05@gmail.com>
2024-01-22 10:37:21 +0200

    commit

---
+++ ADVANCED/Objects and Composition/City Record.js
@@ -0,0 +1,21 @@
+function city(name,population,treasury){
+    const result = {
+        name,
+        population,
+        treasury
+
+    };
+    return result;
+
+}
+
+console.log(city('Tortuga',
+
+7000,
+
+15000));
+console.log(city('Santo Domingo',
+
+12000,
+
+23500))

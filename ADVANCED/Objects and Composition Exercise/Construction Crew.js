c52591ab5055ded3a8d20f49a7a1c2d654d0f063
Daria Sibova <dariasib05@gmail.com>
2024-01-22 11:08:49 +0200

    commit

---
+++ ADVANCED/Objects and Composition Exercise/Construction Crew.js
@@ -0,0 +1,41 @@
+function solve(worker) {
+    if (worker.dizziness) {
+      worker.levelOfHydrated += worker.weight * worker.experience * 0.1;
+      worker.dizziness = !worker.dizziness;
+    }
+    return worker;
+  }
+  console.log(
+    solve({
+      weight: 80,
+  
+      experience: 1,
+  
+      levelOfHydrated: 0,
+  
+      dizziness: true,
+    })
+  );
+  console.log(
+    solve({
+      weight: 120,
+  
+      experience: 20,
+  
+      levelOfHydrated: 200,
+  
+      dizziness: true,
+    })
+  );
+  console.log(
+    solve({
+      weight: 95,
+  
+      experience: 3,
+  
+      levelOfHydrated: 0,
+  
+      dizziness: false,
+    })
+  );
+  

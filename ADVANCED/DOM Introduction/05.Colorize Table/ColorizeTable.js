7d07c98bff7607d8f61aa504efb45cba17fbe914
Daria Sibova <dariasib05@gmail.com>
2024-01-24 16:53:58 +0200

    commit

---
+++ ADVANCED/DOM Introduction/05.Colorize Table/ColorizeTable.js
@@ -0,0 +1,11 @@
+function colorize() {
+    // TODO
+    // const rows = Array.from(document.getElementsByTagName('tr'));
+    // for(let i = 1;i < rows.length;i += 2){
+    //     rows[i].style.background = 'teal';
+    // }
+    const rows = document.querySelectorAll('tr:nth-child(even)');
+    for(let row of rows){
+        row.style.background = 'teal';
+    }
+}
